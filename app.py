bd24851e589570d9a915d103e85cbee96f245a3e
Donavan Bugan - CPC <dbugan@chebelopele.com>
2024-07-05 11:53:04 +0200

    Dev: Modularized File Converter - get column names

---
+++ app.py
@@ -0,0 +1,10 @@
+import glob
+import os
+import json
+import re
+import pandas as pd
+
+def get_column_names(schemas, ds_name, sorting_key='column_position'):
+    column_details = schemas[ds_name]
+    columns = sorted(column_details, key=lambda col: col[sorting_key])
+    return [col['column_name'] for col in columns]
