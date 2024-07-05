630ab352b0153eda742b8fc8dc0cc754bdb5bd66
Donavan Bugan - CPC <dbugan@chebelopele.com>
2024-07-05 12:38:28 +0200

    Dev: Add Logic - Add runtime environment variables  - use environment variables - use environment variables n file format converter - pass JSON array as argument too application - exception handling logic - raise exceptions

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import sys
 import glob
 import os
 import json
 import re
 import pandas as pd
+
 
 def get_column_names(schemas, ds_name, sorting_key='column_position'):
     column_details = schemas[ds_name]
@@ -13,11 +15,9 @@ def get_column_names(schemas, ds_name, sorting_key='column_position'):
 def read_csv(file, schemas):
     file_path_list = re.split('[/\\\]', file)
     ds_name = file_path_list[-2]
-    file_name = file_path_list[-1]
     columns = get_column_names(schemas, ds_name)
     df = pd.read_csv(file, names=columns)
     return df
-
 
 
 def to_json(df, tgt_base_dir, ds_name, file_name):
@@ -27,21 +27,40 @@ def to_json(df, tgt_base_dir, ds_name, file_name):
         json_file_path,
         orient='records',
         lines=True
+    )
 
 
-def file_converter(ds_name):
-    src_base_dir = 'data/retail_db'
-    tgt_base_dir = 'data/retail_db_json'
-
+def file_converter(src_base_dir, tgt_base_dir, ds_name):
     schemas = json.load(open(f'{src_base_dir}/schemas.json'))
     files = glob.glob(f'{src_base_dir}/{ds_name}/part-*')
+    if len(files) == 0:
+        raise NameError(f'No files found for {ds_name}')
 
     for file in files:
-        print(f'Processing {file}')
-        df = read_csv(file, schemas)
-        file_name = re.split('[/\\\]', file)[-1]
-        to_json(df, tgt_base_dir, ds_name, file_name)
+        try:
+            df = read_csv(file, schemas)
+            file_name = re.split('[/\\\]', file)[-1]
+            to_json(df, tgt_base_dir, ds_name, file_name)
+        except NameError as ne:
+            print(ne)
+            print(f'Error processing {ds_name}')
+            pass
 
-ds_name = 'orders'
 
-file_converter(ds_name)
+def process_files(ds_names=None):
+    src_base_dir = os.environ.get('SRC_BASE_DIR')
+    tgt_base_dir = os.environ.get('TGT_BASE_DIR')
+    schemas = json.load(open(f'{src_base_dir}/schemas.json'))
+    if not ds_names:
+        ds_names = schemas.keys()
+    for ds_name in ds_names:
+        print(f'Processing {ds_name}')
+        file_converter(src_base_dir, tgt_base_dir, ds_name)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        ds_names = json.loads(sys.argv[1])
+        process_files(ds_names)
+    else:
+        process_files()
