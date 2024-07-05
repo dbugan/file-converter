535ba5237fe00d76a772d26e053d3833d2ec4cd7
Donavan Bugan - CPC <dbugan@chebelopele.com>
2024-07-05 11:54:04 +0200

    Dev: Modularized File Converter define csv file reader

--- app.py
+++ app.py
@@ -8,3 +8,12 @@ def get_column_names(schemas, ds_name, sorting_key='column_position'):
     column_details = schemas[ds_name]
     columns = sorted(column_details, key=lambda col: col[sorting_key])
     return [col['column_name'] for col in columns]
+
+
+def read_csv(file, schemas):
+    file_path_list = re.split('[/\\\]', file)
+    ds_name = file_path_list[-2]
+    file_name = file_path_list[-1]
+    columns = get_column_names(schemas, ds_name)
+    df = pd.read_csv(file, names=columns)
+    return df
